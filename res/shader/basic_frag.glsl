3babde275d0526085d26fd7b8b98904d09d80ca3
Lehdari <miika.lehtimaki@gmail.com>
2023-07-22 16:55:37 +0300

    basic_frag.glsl: Implement more complex plasma

--- res/shader/basic_frag.glsl
+++ res/shader/basic_frag.glsl
@@ -1,7 +1,6 @@
 #version 410
 
 #include "uniforms.glsl"
-#include "hg_sdf.glsl"
 
 uniform vec3 dColor;
 
@@ -13,8 +12,17 @@ void main()
     if (uTime < -1 || uRes.x < -1)
         discard;
 
-    vec2 uv = gl_FragCoord.xy / uRes.xy;
-    vec3 color = dColor + vec3(uv, 0.5 * sin(uTime) + 0.5);
+    vec2 uv = gl_FragCoord.xy*2.0 / uRes.xy - 1.0;
+
+    vec2 rOff = vec2(sin(uTime*0.432552), cos(uTime*0.127652));
+    vec2 gOff = vec2(sin(uTime*0.245313), cos(uTime*0.628314));
+    vec2 bOff = vec2(sin(uTime*0.120647), cos(uTime*0.394022));
+
+    vec3 color = dColor + vec3(
+        0.5+0.5*sin(length(uv + rOff)*3.4 + 10.0*cos(uTime * 1.0)),
+        0.5+0.5*sin(length(uv + gOff)*2.9 + 10.0*cos(uTime * 1.2)),
+        0.5+0.5*sin(length(uv + bOff)*1.3 + 10.0*cos(uTime * 0.9))
+    );
 
     fragColor = vec4(color, 1);
 }
