6a0c816f524273a01f65a64210d49f2f1ae1ab23
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-07-23 18:45:14 +0300

    text_gen: Take input from stdin to support piping

--- text_gen.py
+++ text_gen.py
@@ -2,12 +2,13 @@
 
 import sys
 
-assert len(sys.argv) == 2
-input = sys.argv[1]
-
+print("Give input as a line, piped or ctrl-D after enter", flush=True)
+input = sys.stdin.readlines()
+assert len(input) == 1, "Only one input line supported"
 
 output_arr = str()
-for c in input:
+
+for c in input[0]:
     if c >= 'a' and c <= 'z':
         output_arr += str(ord(c) - ord('a'))
     elif c == '!':
@@ -18,14 +19,15 @@ for c in input:
         output_arr += "28"
     elif c == '_':
         output_arr += "29"
-    elif c == ' ':
+    elif c == ' ' or c == '\n':
         output_arr += "30"
+        output_arr
     else:
-        print(f"Unsupported char '{c}")
+        print(f"Unsupported char '{c}'")
         output_arr += "99"
     output_arr += ','
 output_arr = output_arr[:-1]
 
-print(f"const int CHAR_COUNT = {len(input)};")
+print(f"const int CHAR_COUNT = {len(input[0])};")
 print("const int CHARS[CHAR_COUNT]= int[](" + output_arr + ");")
 
