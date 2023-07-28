2b753db9f3dc211b541ba4219ad49c42a818e25e
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-07-28 20:44:01 +0300

    tonemap: Use 'aces' instead of uncharted
    A bit more contrast without tweaking values

--- res/shader/tonemap.glsl
+++ res/shader/tonemap.glsl
@@ -2,25 +2,23 @@
 float stol(float x) { return (x <= 0.04045 ? x / 12.92 : pow((x + 0.055) / 1.055, 2.4)); }
 vec3 stol(vec3 c) { return vec3(stol(c.x), stol(c.y), stol(c.z)); }
 
-// From http://filmicworlds.com/blog/filmic-tonemapping-operators/
-// https://www.slideshare.net/ozlael/hable-john-uncharted2-hdr-lighting
-vec3 Uncharted2Tonemap(vec3 color)
+// From Krzysztof Narkowicz
+// https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+vec3 ACESFilm(vec3 x)
 {
-    float A = 0.15; // Shoulder strength
-    float B = 0.50; // Linear strength
-    float C = 0.10; // Linear angle
-    float D = 0.20; // Toe strength
-    float E = 0.02; // Toe numerator
-    float F = 0.30; // Toe denominator
-    return ((color*(A*color+C*B)+D*E)/(color*(A*color+B)+D*F))-E/F;
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+    return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0, 1);
 }
 
 vec3 tonemap(vec3 color)
 {
     float exposure = 1.0;
     float gamma = 2.2;
-    float linearWhite = 11.2;
-    vec3 outcol = Uncharted2Tonemap(color * exposure);
-    outcol /= Uncharted2Tonemap(vec3(linearWhite));
-    return pow(outcol, vec3(1 / gamma));
+    vec3 outColor = ACESFilm(color * exposure);
+    return pow(outColor, vec3(1 / gamma));
 }
+
